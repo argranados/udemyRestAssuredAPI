93ff8393f7e751cec7b53acbb394ba85234bdb67
Alfonso Granados <alfonso.granados@globant.com>
2023-11-21 15:21:06 -0600

    Query Parameters

--- src/test/java/GetBookingIdsTests.java
+++ src/test/java/GetBookingIdsTests.java
@@ -22,4 +22,22 @@ public class GetBookingIdsTests extends BaseTest{
 		Assert.assertFalse(bookingIds.isEmpty(), "List of bookingIds is empty, but it shouldn't be");
 	}
 
+	@Test
+	public void getBookingIdsWithFilterTest() {
+		// add query parameter to spec
+		spec.queryParam("firstname", "Susan");
+		spec.queryParam("lastname", "Brown");
+
+		// Get response with booking ids
+		Response response = RestAssured.given(spec).get("/booking");
+		response.print();
+
+		// Verify response 200
+		Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
+
+		// Verify at least 1 booking id in response
+		List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+		Assert.assertFalse(bookingIds.isEmpty(), "List of bookingIds is empty, but it shouldn't be");
+	}
+
 }
